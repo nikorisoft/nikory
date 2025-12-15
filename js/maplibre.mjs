c6ef83b005d9c0d57ee151ceaac8bf496411c6d3
nikorisoft <44279059+nikorisoft@users.noreply.github.com>
2025-12-15 22:45:05 +0000

    Automatic synchronization with source

--- js/maplibre.mjs
+++ js/maplibre.mjs
@@ -2,8 +2,8 @@
 import "./maplibre-gl.js";
 
 const lineColors = {
-    inactive: "#5d6b89",
-    active: "#f97316",
+    inactive: "rgb(93 107 137 / 50%)",
+    active: "rgb(249 115 22)",
 };
 const lineWidths = {
     inactive: 2,
@@ -36,9 +36,16 @@ function getTimelineItems() {
  * @property {number} lat2
  * @property {number} lon1
  * @property {number} lon2
+ * @property {GeoJSON.GeoJSON | null} track
  */
 /**
  * @typedef {Object} VisitItem
+ * @property {string} id
+ * @property {number} lat
+ * @property {number} lon
+ */
+/**
+ * @typedef {Object} AccommodationsItem
  * @property {string} id
  * @property {number} lat
  * @property {number} lon
@@ -53,6 +60,10 @@ function rebuildNitroxData() {
      * @type { VisitItem []}
      */
     const visits = [];
+    /**
+     * @type { AccommodationsItem []}
+     */
+    const accommodations = [];
 
     const ts = document.querySelectorAll("div.timeline-item[data-l-type='transportation']");
     for (const t of ts) {
@@ -78,12 +89,24 @@ function rebuildNitroxData() {
         const lon1 = parseFloat(attrLon1);
         const lon2 = parseFloat(attrLon2);
 
+        const trackData = t.querySelector(".track-data");
+        let track = null;
+        if (trackData != null) {
+            try {
+                const geoJson = trackData.getAttribute("data-track");
+                if (geoJson != null) {
+                    track = JSON.parse(geoJson);
+                }
+            } catch (e) {}
+        }
+
         transportations.push({
             id,
             lat1,
             lat2,
             lon1,
             lon2,
+            track,
         });
     }
     const vs = document.querySelectorAll("div.timeline-item[data-l-type='visit']");
@@ -109,9 +132,33 @@ function rebuildNitroxData() {
         visits.push({ id, lat, lon });
     }
 
+    const accs = document.querySelectorAll("div.timeline-item[data-l-type='accommodations']");
+    for (const a of accs) {
+        const id = a.getAttribute("data-l-id");
+        if (id == null) {
+            continue;
+        }
+        const loc = a.querySelector(".location-info");
+        if (loc == null) {
+            continue;
+        }
+
+        const attrLat = loc.getAttribute("data-l-lat");
+        const attrLon = loc.getAttribute("data-l-lon");
+        if (attrLat == null || attrLon == null) {
+            continue;
+        }
+
+        const lat = parseFloat(attrLat);
+        const lon = parseFloat(attrLon);
+
+        accommodations.push({ id, lat, lon });
+    }
+
     return {
         transportations,
         visits,
+        accommodations,
     };
 }
 
@@ -134,6 +181,8 @@ class NitroxController {
                 this.inactivateLine(id);
             } else if (type === "visit") {
                 this.inactivatePoint(id);
+            } else if (type === "accommodations") {
+                this.inactivatePoint(id);
             }
         }
 
@@ -146,6 +195,8 @@ class NitroxController {
                 if (type === "transportation") {
                     this.activateLine(id);
                 } else if (type === "visit") {
+                    this.activatePoint(id);
+                } else if (type === "accommodations") {
                     this.activatePoint(id);
                 }
             }
@@ -183,6 +234,45 @@ class NitroxController {
                 },
             },
         });
+
+        // @ts-ignore
+        this.bounds[id] = new maplibregl.LngLatBounds(
+            [Math.min(lon1, lon2), Math.min(lat1, lat2)],
+            [Math.max(lon1, lon2), Math.max(lat1, lat2)],
+        );
+
+        this.map.addLayer({
+            id,
+            type: "line",
+            source: id,
+            layout: {
+                "line-join": "round",
+                "line-cap": "round",
+            },
+            paint: {
+                "line-color": lineColors.inactive,
+                "line-width": lineWidths.inactive,
+            },
+        });
+    }
+
+    /**
+     * @param {string} id
+     * @param {GeoJSON.GeoJSON} geoJson
+     */
+    addGeoJson(id, geoJson) {
+        this.map.addSource(id, {
+            type: "geojson",
+            data: geoJson,
+        });
+
+        // @ts-ignore
+        const bb = new maplibregl.LngLatBounds();
+        // @ts-ignore
+        for (const coord of geoJson.geometry.coordinates) {
+            bb.extend(coord);
+        }
+        this.bounds[id] = bb;
 
         this.map.addLayer({
             id,
@@ -235,18 +325,14 @@ class NitroxController {
         this.map.setPaintProperty(id, "line-color", lineColors.active);
         this.map.setPaintProperty(id, "line-width", lineWidths.active);
 
+        this.map.moveLayer(id);
+
         const t = this.getTransportationById(id);
         if (t != null) {
-            this.map.fitBounds(
-                [
-                    [Math.min(t.lon1, t.lon2), Math.min(t.lat1, t.lat2)],
-                    [Math.max(t.lon1, t.lon2), Math.max(t.lat1, t.lat2)],
-                ],
-                {
-                    padding: { bottom: 40, top: 40, left: 30, right: 30 },
-                    speed: 2,
-                },
-            );
+            this.map.fitBounds(this.bounds[id], {
+                padding: { bottom: 40, top: 40, left: 30, right: 30 },
+                speed: 2,
+            });
         }
     }
     /**
@@ -264,12 +350,22 @@ class NitroxController {
         this.map.setPaintProperty(id, "circle-color", pointColors.active);
         this.map.setPaintProperty(id, "circle-radius", pointRadii.active);
 
+        this.map.moveLayer(id);
+
         const v = this.getVisitById(id);
         if (v != null) {
             this.map.flyTo({
                 center: [v.lon, v.lat],
                 zoom: 13,
             });
+        } else {
+            const a = this.getAccommodationsById(id);
+            if (a != null) {
+                this.map.flyTo({
+                    center: [a.lon, a.lat],
+                    zoom: 13,
+                });
+            }
         }
     }
     /**
@@ -296,16 +392,30 @@ class NitroxController {
     getVisitById(id) {
         return this.visits.find((v) => v.id === id);
     }
-
     /**
      *
+     * @param {string} id
+     * @returns
+     */
+    getAccommodationsById(id) {
+        return this.accommodations.find((a) => a.id === id);
+    }
+
+    /**
      * @param {string} containerId
      * @param {TransportationItem[]} transportations
      * @param {VisitItem[]} visits
+     * @param {AccommodationsItem[]} accommodations
      */
-    constructor(containerId, transportations, visits) {
+    constructor(containerId, transportations, visits, accommodations) {
         this.transportations = transportations;
         this.visits = visits;
+        this.accommodations = accommodations;
+
+        /**
+         * @type {Record<string, maplibregl.LngLatBounds>}
+         */
+        this.bounds = {};
 
         // @ts-ignore
         this.map = new maplibregl.Map({
@@ -318,10 +428,17 @@ class NitroxController {
 
         this.map.on("load", () => {
             for (const t of transportations) {
-                this.addLine(t.id, t.lat1, t.lon1, t.lat2, t.lon2);
+                if (t.track != null) {
+                    this.addGeoJson(t.id, t.track);
+                } else {
+                    this.addLine(t.id, t.lat1, t.lon1, t.lat2, t.lon2);
+                }
             }
             for (const v of visits) {
                 this.addPoint(v.id, v.lat, v.lon);
+            }
+            for (const a of accommodations) {
+                this.addPoint(a.id, a.lat, a.lon);
             }
 
             const observer = new IntersectionObserver(
@@ -354,7 +471,7 @@ class NitroxController {
  * @param {string} containerId
  */
 export function initMap(containerId) {
-    const { transportations, visits } = rebuildNitroxData();
+    const { transportations, visits, accommodations } = rebuildNitroxData();
 
-    const controller = new NitroxController(containerId, transportations, visits);
+    const controller = new NitroxController(containerId, transportations, visits, accommodations);
 }
