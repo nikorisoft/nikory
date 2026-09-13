804a33c99db02794d45bbbf3b1f93b3b46c96526
nikorisoft <44279059+nikorisoft@users.noreply.github.com>
2026-09-13 11:37:36 +0000

    Automatic synchronization with source

--- js/gallery.mjs
+++ js/gallery.mjs
@@ -1,10 +1,37 @@
-import "./glightbox.min.js";
+import PhotoSwipeLightbox from "./photoswipe-lightbox.esm.js";
 
 document.addEventListener("DOMContentLoaded", () => {
-    const lb = GLightbox({
-        touchNavigation: true,
-        loop: true,
-        selector: ".gallery-item",
-        skin: "nikory",
+    const lightbox = new PhotoSwipeLightbox({
+        gallery: ".gallery",
+        children: "a",
+        pswpModule: () => import("./photoswipe.esm.js"),
     });
+
+    lightbox.on("uiRegister", function () {
+        lightbox.pswp.ui.registerElement({
+            name: "custom-caption",
+            order: 9,
+            isButton: false,
+            appendTo: "root",
+            html: "Caption text",
+            onInit: (el, pswp) => {
+                lightbox.pswp.on("change", () => {
+                    const currSlideElement = lightbox.pswp.currSlide.data.element;
+                    let captionHTML = "";
+                    if (currSlideElement) {
+                        // get caption from alt attribute
+                        const caption = currSlideElement.querySelector(".caption");
+                        if (caption) {
+                            captionHTML = caption.innerHTML;
+                        } else {
+                            captionHTML = currSlideElement.querySelector("img").getAttribute("alt");
+                        }
+                    }
+                    el.innerHTML = captionHTML || "";
+                });
+            },
+        });
+    });
+
+    lightbox.init();
 });
